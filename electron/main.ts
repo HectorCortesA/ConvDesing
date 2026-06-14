0a5f2f2f0da5f2503e83bd15f5cb7e195b76c74c
Hector Cortes <hectordroid903@gmail.com>
2026-06-14 04:29:18 -0600

    Fix: Add Version

--- electron/main.ts
+++ electron/main.ts
@@ -37,9 +37,6 @@ function createWindow() {
   }
 }
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
